076f68ca415a0193558173561c8571fda659683b
nu-ch <nuchaiwongthon@gmail.com>
2022-08-09 19:01:31 +0700

    Implement go structure project

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/oldrock-api/auth"
+)
+
+func main() {
+
+	router := gin.New()
+	auth.RegisterHandlers(router)
+
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	router.Run(":8080")
+}
